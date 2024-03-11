7591fa46c17b15ddd4796e26a032bce93876b243
Dean Hudek <hudek.dean@gmail.com>
2024-03-11 21:54:40 +0100

    removed postgis

--- moj_opg/settings.py
+++ moj_opg/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'moj_opg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
